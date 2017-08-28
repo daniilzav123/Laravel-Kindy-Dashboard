cb60df297d1294f49a05395bf6a7525b69086e7c
Kaushik <techwell602@gmail.com>
2017-08-29 04:04:38 +0530

    All images for navigations and font files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,99 +1,103 @@
 @extends('layouts.app')
-<style>
-.nav-tabs li a {
-color: #777;
-}
-</style>
 
 @section('content')
 <div class="container">
-    <!-- <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <section class="content-header set-bg-section">
+        <div class="timeline-body">
+          <a href="" title="Dashboard"><img src="{{asset('images/dashboard.png')}}" alt="..." class=""></a>
+          <a href="" title="Students"><img src="{{asset('images/students.png')}}" alt="..." class=""></a>
+          <a href="" title="Daily Reports"><img src="{{asset('images/reports.png')}}" alt="..." class=""></a>
+          <a href="" title="Policies"><img src="{{asset('images/policies.png')}}" alt="..." class=""></a>
+          <a href="" title="Checklist"><img src="{{asset('images/checklist.png')}}" alt="..." class=""></a>
+          <a href="" title="Rostering"><img src="{{asset('images/rostering.png')}}" alt="..." class=""></a>
         </div>
-    </div>
-     --><!-- <h3 class="text-center">Dashboard</h3> 
-    <ul class="nav nav-tabs">
-      <li class="active"><a data-toggle="tab" href="#home"><img src="{{asset('images/dashboard.png')}}">Dashboard</a></li>
-      <li><a data-toggle="tab" href="#menu1"><img src="{{asset('images/dashboard.png')}}">School</a></li>
-      <li><a data-toggle="tab" href="#menu2"><img src="{{asset('images/dashboard.png')}}">Colleage</a></li>
-      <li><a data-toggle="tab" href="#menu2"><img src="{{asset('images/dashboard.png')}}">Colleage</a></li>
-      <li><a data-toggle="tab" href="#menu2"><img src="{{asset('images/dashboard.png')}}">Colleage</a></li>
-    </ul> -->
+    </section>
 
-    <!-- <div class="tab-content">
-      <div id="home" class="tab-pane fade in active">
-        <h2>Mike Ross, Manager</h2>
-        <p>Man, we've been on the road for some time now. Looking forward to lorem ipsum.</p>
-      </div>
-      <div id="menu1" class="tab-pane fade">
-        <h2>Chandler Bing, Guitarist</h2>
-        <p>Always a pleasure people! Hope you enjoyed it as much as I did. Could I BE.. any more pleased?</p>
-      </div>
-      <div id="menu2" class="tab-pane fade">
-        <h2>Peter Griffin, Bass player</h2>
-        <p>I mean, sometimes I enjoy the show, but other times I enjoy other things.</p>
-      </div>
-    </div> -->
     <h2>Dashboard</h2>
-    <div class="row">
-        <div class="col-md-12 col-sm-6">
-            <div class="panel panel-default">
-                <div class="panel-body" style="background-color: #1c2754;">
-                    <div class="col-md-2">
-                        <a href="#"><img src="{{asset('images/dashboard.png')}}"></a>
-                    </div>
-                    <div class="col-md-2">
-                        <img src="{{asset('images/dashboard.png')}}">
-                    </div>
-                    <div class="col-md-2">
-                        <img src="{{asset('images/dashboard.png')}}">
-                    </div>
-                    <div class="col-md-2">
-                        <img src="{{asset('images/dashboard.png')}}">
-                    </div>
-                    <div class="col-md-2">
-                        <img src="{{asset('images/dashboard.png')}}">
-                    </div>
-                    <div class="col-md-2">
-                        <img src="{{asset('images/dashboard.png')}}">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <h5>View and overview of management client</h5>
     <br/>
     <div class="row">
         <div class="col-sm-4">
           <div class="panel panel-primary">
-            <div class="panel-heading">BLACK FRIDAY DEAL</div>
-            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+            <div class="panel-body"><img src="{{asset('images/dashboard-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-user fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Students</a></h5>
+                    <h6><a href="">Children's profile page</a></h6>
+                </div>
+                </div> 
+            </div>
           </div>
         </div>
-        <div class="col-sm-4"> 
-          <div class="panel panel-danger">
-            <div class="panel-heading">BLACK FRIDAY DEAL</div>
-            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-body"><img src="{{asset('images/reports-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-book fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Daily Reports</a></h5>
+                    <h6><a href="">View various metric reports</a></h6>
+                </div>
+                </div> 
+            </div>
           </div>
         </div>
-        <div class="col-sm-4"> 
-          <div class="panel panel-success">
-            <div class="panel-heading">BLACK FRIDAY DEAL</div>
-            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-body"><img src="{{asset('images/policies-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-file-text-o fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Policies</a></h5>
+                    <h6><a href="">Documents, policies, procedures</a></h6>
+                </div>
+                </div> 
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-body"><img src="{{asset('images/checklist-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-calendar-check-o fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Checklists</a></h5>
+                    <h6><a href="">Student milestonoe, Achivements</a></h6>
+                </div>
+                </div> 
+            </div>
+          </div>
+        </div><div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-body"><img src="{{asset('images/restoring-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-address-book-o fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Rostering</a></h5>
+                    <h6><a href="">Attendance, records, bookkeeping</a></h6>
+                </div>
+                </div> 
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-body"><img src="{{asset('images/students-main.png')}}" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">
+                <div class="display-in-block">
+                <div class="display-in-block"><span class="info-box-icon"><i class="fa fa-user fa-4x" aria-hidden="true"></i></span></div>
+                <div class="display-in-block left-padding">
+                    <h5 class="bold"><a href="">Account</a></h5>
+                    <h6><a href="">View and merge account info</a></h6>
+                </div>
+                </div> 
+            </div>
           </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,47 +4,33 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
+    <title>{{ config('app.name', 'Laravel Demo') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/_all-skins.min.css') }}">
-    
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
-                    <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Laravel Demo') }}
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
@@ -75,8 +61,13 @@
         </nav>
 
         @yield('content')
+        
     </div>
-
+    <div id="footer" class="navbar-default">
+      <div class="container ">
+        <h5 class="text-muted text-right">- Demo application By Kaushik </h5>
+      </div>
+    </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
